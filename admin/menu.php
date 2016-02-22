5580dba84236f213ec02bef01a13c16e1cff6c0c
Saleh Ahmad <salehoyon@hotmail.com>
2016-02-22 23:59:03 +0600

    Update Responsiveness

--- admin/menu.php
+++ admin/menu.php
@@ -1,7 +1,7 @@
 <div id="sidebar-left" class="span2">
     <div class="nav-collapse sidebar-nav">
         <ul class="nav nav-tabs nav-stacked main-menu">
-            <li><a href="index.php"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+            <li><a href="<?php echo SERVER ?>/admin"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>
             <li>
                 <a class="dropmenu" href="#"><i class="icon-shopping-cart"></i><span class="hidden-tablet"> Orders</span></a>
                 <ul>
--- includes/upperHeader.php
+++ includes/upperHeader.php
@@ -6,10 +6,10 @@
  * Time: 2:45 PM
  */?>
 <div class="row">
-    <div class="small-5 columns">
+    <div class="small-4 medium-5 columns">
         <h2>Three Aces</h2>
     </div>
-    <div class="small-5 columns">
+    <div class="small-4 medium-5 columns">
         <p>
             <strong>
                 617 491-2884<br/>
@@ -17,7 +17,7 @@
             </strong>
         </p>
     </div>
-    <div class="small-2 columns">
+    <div class="small-4 medium-2 columns">
         <address>
             <small>
                 1613 Massachusetts<br/>
