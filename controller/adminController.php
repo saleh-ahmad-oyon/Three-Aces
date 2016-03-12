9a1d49673de8fba5b4353e4f32e255343cdec074
Saleh Ahmad <salehoyon@hotmail.com>
2016-03-13 00:33:52 +0600

    spacing and line break

--- controller/adminController.php
+++ controller/adminController.php
@@ -5,41 +5,42 @@
  * Date: 2/19/2016
  * Time: 12:58 PM
  */
-require_once '../model/db.php';
+    require_once '../model/db.php';
 
-if(isset($_POST['day']) && $_POST['day'] == 'today'){
-    $num= todayOrders();
-    echo $num;
-}elseif(isset($_POST['day']) && $_POST['day'] == 'all'){
-    $num = totalOrders();
-    echo $num;
-}elseif(isset($_POST['orderKey'])){
-    $key = $_POST['orderKey'];
-    $row = allInfo($key);
-    echo json_encode($row);
-}elseif(isset($_POST['oldpa'])){
-    $old = $_POST['oldpa'];
-    $new = $_POST['newpa'];
-    $confirm = $_POST['confirmpa'];
-    $key = $_POST['key'];
-    if(checkOldPass($old)){
-        if($new == $confirm){
-            updatePass($key, $new);
-            echo 'Password Successfully Updated';
-        }else{
-            echo "New password and Confirm Password didn't match !!";
+    if (isset($_POST['day']) && $_POST['day'] == 'today') {
+        $num= todayOrders();
+        echo $num;
+    } elseif (isset($_POST['day']) && $_POST['day'] == 'all') {
+        $num = totalOrders();
+        echo $num;
+    } elseif (isset($_POST['orderKey'])) {
+        $key = $_POST['orderKey'];
+        $row = allInfo($key);
+        echo json_encode($row);
+    } elseif (isset($_POST['oldpa'])) {
+        $old = $_POST['oldpa'];
+        $new = $_POST['newpa'];
+        $confirm = $_POST['confirmpa'];
+        $key = $_POST['key'];
+
+        if(checkOldPass($old)){
+            if($new == $confirm){
+                updatePass($key, $new);
+                echo 'Password Successfully Updated';
+            } else {
+                echo "New password and Confirm Password didn't match !!";
+            }
+        } else {
+            echo "Old Password didn't match";
         }
-    }else{
-        echo "Old Password didn't match";
-    }
-}elseif(isset($_POST['editKey'])) {
-    $key = $_POST['editKey'];
-    $name = $_POST['editName'];
-    $username = $_POST['editUsername'];
-    $email = $_POST['editEmail'];
-    $country = $_POST['editCountry'];
+    } elseif (isset($_POST['editKey'])) {
+        $key = $_POST['editKey'];
+        $name = $_POST['editName'];
+        $username = $_POST['editUsername'];
+        $email = $_POST['editEmail'];
+        $country = $_POST['editCountry'];
 
-    $row = getUserNameEmail($key);
+        $row = getUserNameEmail($key);
 
     if ($row['username'] != $username){
         if (checkUser($username)) {
