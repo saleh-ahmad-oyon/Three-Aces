12be2715eae0d19434b3eb493c7cb5592d37c3e2
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-24 18:31:40 +0600

    Updating...

--- includes/slider.php
+++ includes/slider.php
@@ -1,12 +1,15 @@
-<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit data-use-m-u-i="false" data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
+<div
+    class="orbit"
+    data-orbit
+    data-use-m-u-i="false"
+    data-timer-delay="2000"
+    data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
     <ul class="orbit-container">
-        <button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
-        <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
+        <!--<button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
+        <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>-->
         <li class="orbit-slide is-active">
-            <div class="text-center">
-                <img src="img/calzone.jpg" alt="Calzone" width="100%">
-                <figcaption class="orbit-caption">Calzone</figcaption>
-            </div>
+            <img src="img/calzone.jpg" alt="Calzone" width="100%">
+            <figcaption class="orbit-caption">Calzone</figcaption>
         </li>
         <li class="orbit-slide">
             <img src="img/grinders.jpg" alt="Grinders" width="100%">
@@ -45,7 +48,7 @@
             <figcaption class="orbit-caption">Wraps</figcaption>
         </li>
     </ul>
-    <nav class="orbit-bullets">
+    <!--<nav class="orbit-bullets">
         <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span></button>
         <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
         <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
@@ -56,5 +59,5 @@
         <button data-slide="7"><span class="show-for-sr">Eighth slide details.</span></button>
         <button data-slide="8"><span class="show-for-sr">Ninth slide details.</span></button>
         <button data-slide="9"><span class="show-for-sr">Tenth slide details.</span></button>
-    </nav>
+    </nav>-->
 </div>
