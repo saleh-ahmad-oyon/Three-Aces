203357a13357fa0726f54d3d5e907e80d7431322
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-24 22:38:15 +0600

    updating...

--- includes/slider.php
+++ includes/slider.php
@@ -5,8 +5,6 @@
     data-timer-delay="2000"
     data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
     <ul class="orbit-container">
-        <!--<button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
-        <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>-->
         <li class="orbit-slide is-active">
             <img src="<?= SERVER ?>/img/Calzone.jpg" alt="Calzone" width="100%">
             <figcaption class="orbit-caption">Calzone</figcaption>
@@ -48,16 +46,4 @@
             <figcaption class="orbit-caption">Wraps</figcaption>
         </li>
     </ul>
-    <!--<nav class="orbit-bullets">
-        <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span></button>
-        <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-        <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-        <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-        <button data-slide="4"><span class="show-for-sr">Fifth slide details.</span></button>
-        <button data-slide="5"><span class="show-for-sr">Sixth slide details.</span></button>
-        <button data-slide="6"><span class="show-for-sr">Seventh slide details.</span></button>
-        <button data-slide="7"><span class="show-for-sr">Eighth slide details.</span></button>
-        <button data-slide="8"><span class="show-for-sr">Ninth slide details.</span></button>
-        <button data-slide="9"><span class="show-for-sr">Tenth slide details.</span></button>
-    </nav>-->
 </div>
