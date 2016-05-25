983fcde986716c68a81a0168af845eb11bc97cc2
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-25 14:13:15 +0600

    Updating...

--- controller/admin-login-success.php
+++ controller/admin-login-success.php
@@ -1,33 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Oyon
- * Date: 2/18/2016
- * Time: 12:05 PM
- */
 
-    #list of required files
+/** Required Files */
+require_once 'define.php';
+require_once '../model/db.php';
 
-    require_once 'define.php';
-    require_once '../model/db.php';
-    #
+/** Session Starts */
+session_start();
 
-    session_start();
+if (isset($_POST['loginSubmit'])) {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
 
-    if (isset($_POST['loginSubmit'])) {
-        $user= $_POST['username'];
-        $pass = $_POST['password'];
-
-        if ($user =='' || $pass =='') {
-            header('Location: '.SERVER.'/admin?err=1');
+    if ($user == '' || $pass == '') {
+        header('Location: '.SERVER.'/admin?err=1');
+    } else {
+        if (checkPass($user, $pass)) {
+            $_SESSION['user'] = $user;
+            $_SESSION['name'] = getUserName($user);
+            $_SESSION['id']   = getUserId($user);
+            header('Location: '.SERVER.'/admin');
         } else {
-            if (checkPass($user, $pass)) {
-                $_SESSION['user'] = $user;
-                $_SESSION['name'] = getUserName($user);
-                $_SESSION['id'] = getUserId($user);
-                header('Location: '.SERVER.'/admin');
-            } else {
-                header('Location: '.SERVER.'/admin?err=2');
-            }
+            header('Location: '.SERVER.'/admin?err=2');
         }
     }
+}
--- controller/adminController.php
+++ controller/adminController.php
@@ -1,92 +1,122 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Oyon
- * Date: 2/19/2016
- * Time: 12:58 PM
- */
-    require_once '../model/db.php';
 
-    if (isset($_POST['day']) && $_POST['day'] == 'today') {
-        $num= todayOrders();
-        echo $num;
-    } elseif (isset($_POST['day']) && $_POST['day'] == 'all') {
-        $num = totalOrders();
-        echo $num;
-    } elseif (isset($_POST['orderKey'])) {
-        $key = $_POST['orderKey'];
-        $row = allInfo($key);
-        echo json_encode($row);
-    } elseif (isset($_POST['oldpa'])) {
-        $old = $_POST['oldpa'];
-        $new = $_POST['newpa'];
-        $confirm = $_POST['confirmpa'];
-        $key = $_POST['key'];
+/** Required Files */
+require_once '../model/db.php';
 
-        if(checkOldPass($old)){
-            if($new == $confirm){
-                updatePass($key, $new);
-                echo 'Password Successfully Updated';
-            } else {
-                echo "New password and Confirm Password didn't match !!";
-            }
+if (isset($_POST['day']) && $_POST['day'] == 'today') {
+    $num= todayOrders();
+    echo $num;
+} elseif (isset($_POST['day']) && $_POST['day'] == 'all') {
+    $num = totalOrders();
+    echo $num;
+} elseif (isset($_POST['orderKey'])) {
+    $key = $_POST['orderKey'];
+    $row = allInfo($key);
+    echo json_encode($row);
+} elseif (isset($_POST['oldpa'])) {
+    $old = $_POST['oldpa'];
+    $new = $_POST['newpa'];
+    $confirm = $_POST['confirmpa'];
+    $key = $_POST['key'];
+
+    if (checkOldPass($old)) {
+        if ($new == $confirm) {
+            updatePass($key, $new);
+            echo 'Password Successfully Updated';
         } else {
-            echo "Old Password didn't match";
+            echo "New password and Confirm Password didn't match !!";
         }
-    } elseif (isset($_POST['editKey'])) {
-        $key = $_POST['editKey'];
-        $name = $_POST['editName'];
-        $username = $_POST['editUsername'];
-        $email = $_POST['editEmail'];
-        $country = $_POST['editCountry'];
+    } else {
+        echo "Old Password didn't match";
+    }
+} elseif (isset($_POST['editKey'])) {
+    $key      = $_POST['editKey'];
+    $name     = $_POST['editName'];
+    $username = $_POST['editUsername'];
+    $email    = $_POST['editEmail'];
+    $country  = $_POST['editCountry'];
 
-        $row = getUserNameEmail($key);
+    $row = getUserNameEmail($key);
 
-        if ($row['username'] != $username){
-            if (checkUser($username)) {
-                if ($row['email'] != $email) {
-                    if (checkEmail($email)) {
-                        updateProfile($name, $username, $email, $country, $key);
-                        echo 't';
-                    } else {
-                        echo 'Email Must be Unique';
-                    }
-                } else {
+    if ($row['username'] != $username){
+        if (checkUser($username)) {
+            if ($row['email'] != $email) {
+                if (checkEmail($email)) {
                     updateProfile($name, $username, $email, $country, $key);
                     echo 't';
+                } else {
+                    echo 'Email Must be Unique';
                 }
             } else {
-                echo 'Username Must be Unique';
-            }
-        } elseif ($row['email'] != $email) {
-            if (checkEmail($email)) {
                 updateProfile($name, $username, $email, $country, $key);
                 echo 't';
-            } else {
-                echo 'Email Must be Unique';
             }
         } else {
+            echo 'Username Must be Unique';
+        }
+    } elseif ($row['email'] != $email) {
+        if (checkEmail($email)) {
             updateProfile($name, $username, $email, $country, $key);
             echo 't';
+        } else {
+            echo 'Email Must be Unique';
         }
+    } else {
+        updateProfile($name, $username, $email, $country, $key);
+        echo 't';
     }
-    function getAllOrdersInfo()
-    {
-        $row = allOrdersInfo();
-        return $row;
-    }
-    function getTodayOrdersInfo()
-    {
-        $row = todayOrdersInfo();
-        return $row;
-    }
-    function admininfo($id)
-    {
-        $row = getAdminInfo($id);
-        return $row;
-    }
-    function getCountries()
-    {
-        $row = getCountryName();
-        return $row;
-    }
+}
+
+/**
+ * All Orders' Information
+ *
+ * Fetch all the Orders' information from the Model
+ *
+ * @return array Returns the information of all Orders
+ */
+function getAllOrdersInfo()
+{
+    $row = allOrdersInfo();
+    return $row;
+}
+
+/**
+ * Today's Orders' Information
+ *
+ * Fetch all the Orders' information of today from the Model
+ *
+ * @return array Returns the information of Today's all Orders
+ */
+function getTodayOrdersInfo()
+{
+    $row = todayOrdersInfo();
+    return $row;
+}
+
+/**
+ * Admin Information
+ *
+ * Fetch all the information of the admin from Model using his id
+ *
+ * @param $id    Admin ID
+ *
+ * @return array   Returns all informations of the admin
+ */
+function admininfo($id)
+{
+    $row = getAdminInfo($id);
+    return $row;
+}
+
+/**
+ * All Countries
+ *
+ * Fetch all Countries' Name of the world from Model
+ *
+ * @return array   Returns all Countries' Name
+ */
+function getCountries()
+{
+    $row = getCountryName();
+    return $row;
+}
