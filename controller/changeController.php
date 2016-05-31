21cf54c3b984ee27fb78c8fad4edadfb574fd745
Saleh Ahmad <salehoyon@hotmail.com>
2016-05-31 16:20:41 +0600

    Updating...

--- controller/changeController.php
+++ controller/changeController.php
@@ -9,45 +9,47 @@
 require_once '../model/db.php';
 $resp = array();
 
-if(isset($_POST['calzoneName'])){
+if (isset($_POST['calzoneName'])) {
     $name = $_POST['calzoneName'];
     $cost = $_POST['calzoneCost'];
-    if($_POST['calzoneAction'] == 'add'){
+
+    if ($_POST['calzoneAction'] == 'add') {
         insertCalzone($name, $cost);
-    }else{
+    } else {
         $key = $_POST['calzoneAction'];
         editCalzone($name, $cost, $key);
     }
     echo json_encode($resp);
-}elseif(isset($_POST['editKey'])){
-    $key = $_POST['editKey'];
-    $row = findCalzoneRow($key);
+} elseif (isset($_POST['editKey'])) {
+    $key  = $_POST['editKey'];
+    $row  = findCalzoneRow($key);
     $resp = $row;
     echo json_encode($resp);
-}elseif(isset($_POST['deleteKey'])){
+} elseif (isset($_POST['deleteKey'])) {
     $key = $_POST['deleteKey'];
     deleteCalzoneRow($key);
-}elseif(isset($_POST['editLasagnaKey'])){
-    $key = $_POST['editLasagnaKey'];
-    $row = findLasagnaRow($key);
+} elseif (isset($_POST['editLasagnaKey'])) {
+    $key  = $_POST['editLasagnaKey'];
+    $row  = findLasagnaRow($key);
     $resp = $row;
     echo json_encode($resp);
-}elseif(isset($_POST['deleteLasagnaKey'])){
+} elseif (isset($_POST['deleteLasagnaKey'])) {
     $key = $_POST['deleteLasagnaKey'];
     deleteLasagnaRow($key);
-}elseif(isset($_POST['lasagnaName'])){
+} elseif(isset($_POST['lasagnaName'])) {
     $name = $_POST['lasagnaName'];
     $cost = $_POST['lasagnaCost'];
-    if($_POST['lasagnaAction'] == 'add'){
+
+    if ($_POST['lasagnaAction'] == 'add') {
         insertLasagna($name, $cost);
-    }else{
+    } else {
         $key = $_POST['lasagnaAction'];
         editLasagna($name, $cost, $key);
     }
     echo json_encode($resp);
-}elseif(isset($_POST['editSpeghettiKey'])){
-    $key = $_POST['editSpeghettiKey'];
-    $row = findSpeghettiRow($key);
+} elseif (isset($_POST['editSpeghettiKey'])) {
+    $key  = $_POST['editSpeghettiKey'];
+    $row  = findSpeghettiRow($key);
     $resp = $row;
     echo json_encode($resp);
 }elseif(isset($_POST['deleteSpaghettiKey'])){
