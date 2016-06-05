e38ff73ee76a81a7464e0a4341b1527e5a187784
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-05 11:43:03 +0600

    v1.0

--- controller/admin-login-success.php
+++ controller/admin-login-success.php
@@ -11,20 +11,43 @@ require_once '../model/db.php';
 /** Session Starts */
 session_start();
 
-if (isset($_POST['loginSubmit'])) {
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-
-    if ($user == '' || $pass == '') {
-        header('Location: '.SERVER.'/admin?err=1');
-    } else {
-        if (checkPass($user, $pass)) {
-            $_SESSION['user'] = $user;
-            $_SESSION['name'] = getUserName($user);
-            $_SESSION['id']   = getUserId($user);
-            header('Location: '.SERVER.'/admin');
-        } else {
-            header('Location: '.SERVER.'/admin?err=2');
-        }
-    }
+/**
+ * If $_POST['loginSubmit'] is not set then
+ * it will redirect to the 404 page
+ */
+if (!isset($_POST['loginSubmit'])) {
+    header('Location: '. SERVER.'/404');
+    return;
 }
+
+/**
+ * @var string $user     Username of admin
+ * @var string $pass     Password of that admin
+ */
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+/** Check if the username and password is null or not */
+if ($user == '' || $pass == '') {
+    header('Location: '.SERVER.'/admin?err=1');
+    return;
+}
+
+/** Verify the Username with Password */
+if (!checkPass($user, $pass)) {
+    header('Location: '.SERVER.'/admin?err=2');
+    return;
+}
+
+/**
+ * Session Variables:
+ * @var string $_SESSION['user']     Contains Username of the admin
+ * @var string $_SESSION['name']     Contains Name of the admin
+ * @var string $_SESSION['id']       Contains ID of the admin
+ */
+$_SESSION['user'] = $user;
+$_SESSION['name'] = getUserName($user);
+$_SESSION['id']   = getUserId($user);
+
+/** Redirect to the Admin Home Page */
+header('Location: '.SERVER.'/admin');
