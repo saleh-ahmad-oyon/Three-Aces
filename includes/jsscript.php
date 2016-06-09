25619c57b467c050c952bd98480ebeeea04bc598
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-10 05:39:58 +0600

    v1.0

---
+++ includes/jsscript.php
@@ -0,0 +1,17 @@
+<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-2.2.4.min.js"><\/script>')</script>
+<script src="<?= SERVER ?>/js/plugins.js"></script>
+<script src="<?= SERVER ?>/js/main.js"></script>
+<script src="<?= SERVER ?>/js/app.js"></script>
+<script src="<?= SERVER ?>/js/foundation.min.js"></script>
+<script src="<?= SERVER ?>/js/foundation.util.mediaQuery.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
--- specialitypizza.php
+++ specialitypizza.php
@@ -77,22 +77,6 @@ $row =getSpecialPizza();
             <?php include_once 'includes/footer.php';?>
         </footer>
 
-        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.2.4.min.js"><\/script>')</script>
-        <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script>
-        <script src="js/app.js"></script>
-        <script src="js/foundation.min.js"></script>
-        <script src="js/foundation.util.mediaQuery.js"></script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
+        <?php include_once 'includes/jsscript.php';?>
     </body>
 </html>
