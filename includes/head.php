b59895e6131011e694aa0153b1bee9526a204a03
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-09 19:54:37 +0600

    v1.0

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,10 @@
 <meta charset="utf-8">
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="description" content="It is a demo php project that can be used as a online food booking system. People can easily book the food items using this. It was done by a student of AIUB">
-<meta name="keywords" content="Lonely,L0onely,noob lonely,aiub,AIUB,noob,nooblonely,American International University Bangladesh(AIUB),American International University-Bangladesh,PHP,PHP Project,php demo project,demo project,Bootstrap,Bootstrap Used Project,bootstrap with php,php project using php,Project in AIUB,project done by aiubian,foundation,foundation used project,php project using foundation,foundation with php,online food booking system,food booking system,food booking online,noob project,lonely project,noob lonely project" />
+<meta name="keywords" content="Lonely,L0onely,noob lonely,aiub,AIUB,noob,nooblonely,American International University Bangladesh(AIUB),American International University-Bangladesh,
+PHP,PHP Project,php demo project,demo project,Bootstrap,Bootstrap Used Project,bootstrap with php,php project using php,Project in AIUB,project done by aiubian,foundation,foundation used project,
+php project using foundation,foundation with php,online food booking system,food booking system,food booking online,noob project,lonely project,noob lonely project,html5,html 5,html5 boilerplate,
+html5 boilerplate with php,html5 boilerplate with foundation,html5 boilerplate with php project,html5 boilerplate project,three aces demo project,three aces project" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="language" content="English" />
 <meta name="country" content="Bangladesh" />
