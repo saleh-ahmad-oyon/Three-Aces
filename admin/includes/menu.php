7169a2f037f2d44ec8673d00cbc7b165dfbf1ef2
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-13 05:36:19 +0600

    v1.0

--- admin/includes/menu.php
+++ admin/includes/menu.php
@@ -1,27 +1,55 @@
 <div id="sidebar-left" class="span2">
     <div class="nav-collapse sidebar-nav">
         <ul class="nav nav-tabs nav-stacked main-menu">
-            <li><a href="<?php echo SERVER ?>/admin"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>
             <li>
-                <a class="dropmenu" href="#"><i class="icon-shopping-cart"></i><span class="hidden-tablet"> Orders</span></a>
+                <a href="<?php echo SERVER ?>/admin">
+                    <i class="icon-bar-chart"></i>
+                    <span class="hidden-tablet"> Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a class="dropmenu" href="#">
+                    <i class="icon-shopping-cart"></i>
+                    <span class="hidden-tablet"> Orders</span>
+                    <span class="label label-important"> 2 </span>
+                </a>
                 <ul>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/orders/today "><span class="hidden-tablet"> Today's Orders</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/orders/all"><span class="hidden-tablet"> All Orders</span></a></li>
+                    <li>
+                        <a class="submenu" href="<?php echo SERVER ?>/admin/orders/today ">
+                            <i class="icon-credit-card" title="Today's Orders"></i>
+                            <span class="hidden-tablet"> Today's Orders</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="submenu" href="<?php echo SERVER ?>/admin/orders/all">
+                            <i class="icon-credit-card" title="All Orders"></i>
+                            <span class="hidden-tablet"> All Orders</span>
+                        </a>
+                    </li>
                 </ul>
             </li>
             <li>
-                <a class="dropmenu" href="#"><i class="icon-food"></i><span class="hidden-tablet"> Food Menu</span><span class="label label-important"> 10 </span></a>
+                <a class="dropmenu" href="#">
+                    <i class="icon-food"></i>
+                    <span class="hidden-tablet"> Food Menu</span>
+                    <span class="label label-important"> 10 </span>
+                </a>
                 <ul>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/calzone"><span class="hidden-tablet"> Calzone</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/grinder"><span class="hidden-tablet"> Grinder</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/lasagna"><span class="hidden-tablet"> Lasagna</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/pizza"><span class="hidden-tablet"> Pizza</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/salad"><span class="hidden-tablet"> Salad</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/sideorders"><span class="hidden-tablet"> Side Orders</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/spaghetti"><span class="hidden-tablet"> Spaghetti</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/wraps"><span class="hidden-tablet"> Wraps</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/specialdinner"><span class="hidden-tablet"> Special Dinner</span></a></li>
-                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/specialitypizza"><span class="hidden-tablet"> Speciality Pizza</span></a></li>
+                    <li>
+                        <a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/calzone">
+                            <i class="icon-fire" title="Calzone"></i>
+                            <span class="hidden-tablet"> Calzone</span>
+                        </a>
+                    </li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/grinder"><i class="icon-fire" title="Grinder"></i><span class="hidden-tablet"> Grinder</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/lasagna"><i class="icon-fire" title="Lasagna"></i><span class="hidden-tablet"> Lasagna</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/pizza"><i class="icon-fire" title="Pizza"></i><span class="hidden-tablet"> Pizza</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/salad"><i class="icon-fire" title="Salad"></i><span class="hidden-tablet"> Salad</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/sideorders"><i class="icon-fire" title="Side Orders"></i><span class="hidden-tablet"> Side Orders</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/spaghetti"><i class="icon-fire" title="Spaghetti"></i><span class="hidden-tablet"> Spaghetti</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/wraps"><i class="icon-fire" title="Wraps"></i><span class="hidden-tablet"> Wraps</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/specialdinner"><i class="icon-fire" title="Special Dinner"></i><span class="hidden-tablet"> Special Dinner</span></a></li>
+                    <li><a class="submenu" href="<?php echo SERVER ?>/admin/menuitem/specialitypizza"><i class="icon-fire" title="Speciality Pizza"></i><span class="hidden-tablet"> Speciality Pizza</span></a></li>
                 </ul>
             </li>
         </ul>
