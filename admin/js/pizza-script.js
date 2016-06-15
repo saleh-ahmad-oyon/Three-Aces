3d2b7bc470937ef3d6202d7add3e4357bcc7e774
Saleh Ahmad <salehoyon@hotmail.com>
2016-06-15 15:32:42 +0600

    v1.0

--- admin/js/pizza-script.js
+++ admin/js/pizza-script.js
@@ -1,5 +1,27 @@
 var $pizza = {
-    submitURL : $url.root() + '/controller/changeController'
+    submitURL : $url.root() + '/controller/changeController',
+    updatedTable : function (response) {
+        $('.datatable').DataTable().destroy();
+        $('.dataTable-tbody').html('');
+        var $small = '';
+        for(i=0; i<response.length; ++i){
+            $small = (response[i].pizza_small_price != null) ? '$ ' + response[i].pizza_small_price : '';
+            var content = '<tr class="tableRow" data-id="' + response[i].pizza_id + '">' +
+                '<td>' + (i+1) + '</td>' +
+                '<td>' + response[i].pizza_name + '</td>' +
+                '<td>' + $small + '</td>' +
+                '<td>$ ' + response[i].pizza_large_price + '</td>' +
+                '<td>' +
+                '<div class="text-center">' +
+                '<button class="btn btn-info editPizza" title="Edit"><i class="halflings-icon white edit"></i> Edit</button>&nbsp;' +
+                '<button class="btn btn-danger dltPizza" title="Delete"><i class="halflings-icon white trash"></i> Delete</button>' +
+                '</div>' +
+                '</td>' +
+                '</tr>';
+            $('.dataTable-tbody').append(content);
+        }
+        initDataTable();
+    }
 };
 
 $('.tableRow').each(function (i) {
@@ -42,26 +64,7 @@ $('#addbtn').click(function(){
                 $('.errorItem').modal('show');
             },
             success: function(response){
-                $('.datatable').DataTable().destroy();
-                $('.dataTable-tbody').html('');
-                var $small = '';
-                for(i=0; i<response.length; ++i){
-                    $small = (response[i].pizza_small_price != null) ? '$ ' + response[i].pizza_small_price : '';
-                    var content = '<tr class="tableRow" data-id="' + response[i].pizza_id + '">' +
-                        '<td>' + (i+1) + '</td>' +
-                        '<td>' + response[i].pizza_name + '</td>' +
-                        '<td>' + $small + '</td>' +
-                        '<td>$ ' + response[i].pizza_large_price + '</td>' +
-                        '<td>' +
-                        '<div class="text-center">' +
-                        '<button class="btn btn-info editPizza" title="Edit"><i class="halflings-icon white edit"></i> Edit</button>&nbsp;' +
-                        '<button class="btn btn-danger dltPizza" title="Delete"><i class="halflings-icon white trash"></i> Delete</button>' +
-                        '</div>' +
-                        '</td>' +
-                        '</tr>';
-                    $('.dataTable-tbody').append(content);
-                }
-                initDataTable();
+                $pizza.updatedTable(response);
                 $('.addItem').modal('hide');
                 $('.saveItem').modal('show');
             }
@@ -109,26 +112,7 @@ $(document).on('click', '.dltPizza', function(){
                 $('.errorItem').modal('show');
             },
             success: function(response){
-                $('.datatable').DataTable().destroy();
-                $('.dataTable-tbody').html('');
-                var $small = '';
-                for(i=0; i<response.length; ++i){
-                    $small = (response[i].pizza_small_price != null) ? '$ ' + response[i].pizza_small_price : '';
-                    var content = '<tr class="tableRow" data-id="' + response[i].pizza_id + '">' +
-                        '<td>' + (i+1) + '</td>' +
-                        '<td>' + response[i].pizza_name + '</td>' +
-                        '<td>' + $small + '</td>' +
-                        '<td>$ ' + response[i].pizza_large_price + '</td>' +
-                        '<td>' +
-                        '<div class="text-center">' +
-                        '<button class="btn btn-info editPizza" title="Edit"><i class="halflings-icon white edit"></i> Edit</button>&nbsp;' +
-                        '<button class="btn btn-danger dltPizza" title="Delete"><i class="halflings-icon white trash"></i> Delete</button>' +
-                        '</div>' +
-                        '</td>' +
-                        '</tr>';
-                    $('.dataTable-tbody').append(content);
-                }
-                initDataTable();
+                $pizza.updatedTable(response);
                 $('.deleteItem').modal('hide');
                 $('.dltSuccess').modal('show');
             }
